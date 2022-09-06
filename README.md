f62b05a81d606c06bd9ac0a81bd89aa234e1a693
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-09-06 22:47:41 +0200

    support multiple start/stops

--- README.md
+++ README.md
@@ -67,3 +67,28 @@ with kp.Stopwatch() as sw:
 print(f"operation took {sw.elapsed_milliseconds} milliseconds")
 print(f"operation took {sw.elapsed_seconds} seconds")
 ```
+
+
+#### Time subparts
+```
+import kappaprofiler as kp
+import time
+
+sw1 = kp.Stopwatch()
+sw2 = kp.Stopwatch()
+
+for i in range(1, 3):
+    with sw1:
+        # operation1
+        time.sleep(0.1 * i)
+    with sw2:
+        # operation2
+        time.sleep(0.2 * i)
+
+print(f"operation1 took {sw1.elapsed_seconds:.2f} seconds (average {sw1.average_lap_time:.2f})")
+print(f"operation2 took {sw2.elapsed_seconds:.2f} seconds (average {sw2.average_lap_time:.2f})")
+```
+```
+operation1 took 0.32 seconds (average 0.16)
+operation2 took 0.61 seconds (average 0.30)
+```
--- kappaprofiler/stopwatch.py
+++ kappaprofiler/stopwatch.py
@@ -3,8 +3,8 @@ from .time_provider import TimeProvider
 class Stopwatch:
     def __init__(self, time_provider=None):
         self._start_time = None
-        self._elapsed_seconds = None
-        self._elapsed_seconds_laps = []
+        self._elapsed_seconds = []
+        self._lap_start_time = None
         self._time_provider = time_provider or TimeProvider()
 
     def start(self):
@@ -14,36 +14,35 @@ class Stopwatch:
 
     def stop(self):
         assert self._start_time is not None, "can't stop a stopped stopwatch"
-        self._elapsed_seconds = self._time_provider.time() - self._start_time
+        self._elapsed_seconds.append(self._time_provider.time() - self._start_time)
         self._start_time = None
-        return self._elapsed_seconds
+        self._lap_start_time = None
+        return self._elapsed_seconds[-1]
 
     def lap(self):
         assert self._start_time is not None, "lap requires stopwatch to be started"
-        if len(self._elapsed_seconds_laps) > 0:
-            lap_time = self._time_provider.time() - self._start_time - sum(self._elapsed_seconds_laps)
-        else:
+        if self._lap_start_time is None:
+            # first lap
             lap_time = self._time_provider.time() - self._start_time
-        self._elapsed_seconds_laps.append(lap_time)
+        else:
+            lap_time = self._time_provider.time() - self._lap_start_time
+        self._elapsed_seconds.append(lap_time)
+        self._lap_start_time = self._time_provider.time()
         return lap_time
 
     @property
-    def elapsed_seconds_laps(self):
-        return self._elapsed_seconds_laps
-
-    @property
     def last_lap_time(self):
-        assert len(self._elapsed_seconds_laps) > 0, "last_lap_time requires lap() to be called at least once"
-        return self._elapsed_seconds_laps[-1]
+        assert len(self._elapsed_seconds) > 0, "last_lap_time requires lap()/stop() to be called at least once"
+        return self._elapsed_seconds[-1]
 
     @property
     def lap_count(self):
-        return len(self._elapsed_seconds_laps)
+        return len(self._elapsed_seconds)
 
     @property
     def average_lap_time(self):
-        assert len(self._elapsed_seconds_laps) > 0, "last_lap_time requires lap() to be called at least once"
-        return sum(self._elapsed_seconds_laps) / len(self._elapsed_seconds_laps)
+        assert len(self._elapsed_seconds) > 0, "average_lap_time requires lap()/stop() to be called at least once"
+        return sum(self._elapsed_seconds) / len(self._elapsed_seconds)
 
     def __enter__(self):
         self.start()
@@ -53,9 +52,14 @@ class Stopwatch:
         self.stop()
 
     @property
+    def elapsed_time(self):
+        return self.elapsed_seconds
+
+    @property
     def elapsed_seconds(self):
-        assert self._elapsed_seconds is not None, "stopwatch has to be started and stopped before a time can be read"
-        return self._elapsed_seconds
+        assert self._start_time is None, "elapsed_seconds requires stopwatch to be stopped"
+        assert len(self._elapsed_seconds) > 0, "elapsed_seconds requires stopwatch to have been started and stopped"
+        return sum(self._elapsed_seconds)
 
     @property
     def elapsed_milliseconds(self):
